e5138014b491349da2514133cdb849f24a769c78
lysander051 <badet.maxime@gmail.com>
2022-04-06 15:37:01 +0200

    ajout

--- GestionAventure.ml
+++ GestionAventure.ml
@@ -5,8 +5,13 @@ let read = fun s ->
   read_line();;
 let init_aventure = fun ()->
   let n = read("Ton nom: ") in
-  let s = read("Ton genre: ") in
-  let c = read("Ta classe: ") in
+  let s = read("\n\nTon genre: 
+                F) Femme
+                H) Homme") in
+  let c = read("\n\nTa classe: 
+                A) Archer
+                G) Guerrier
+                M) Mage") in
   print_string (n ^ s ^c);;
 ;;
 
