16a0c58b03c406de4a5a7eac65016dfe89c17db0
lysander051 <badet.maxime@gmail.com>
2022-04-06 16:59:44 +0200

    init_aventure

--- GestionAventure.ml
+++ GestionAventure.ml
@@ -1,18 +1,47 @@
 open Personnage;;
 
-let read = fun s ->
-  let () = print_string s in
-  read_line();;
-let init_aventure = fun ()->
-  let n = read("Ton nom: ") in
-  let s = read("\n\nTon genre: 
-                F) Femme
-                H) Homme") in
-  let c = read("\n\nTa classe: 
-                A) Archer
-                G) Guerrier
-                M) Mage") in
-  print_string (n ^ s ^c);;
-;;
+exception Lecture_Creation_Perso;;
+let rec read_nom = fun () ->
+  let () = print_string "Ton nom: " in
+  let n = read_line() in
+  if n="" then (print_string "nom incorrecte\n\n\n"; read_nom())
+  else n;;
 
+let rec read_genre = fun () ->
+  let () = print_string "
+Ton genre: 
+  F) Femme
+  H) Homme
+Votre choix: " in
+let g=read_line() in
+if not(g="F" || g="H") then (print_string "tu DOIS avoir un genre\n\n"; read_genre())
+else g;;
+
+let rec read_classe = fun g ->
+  let () = 
+  if g = "H" then 
+    print_string "
+Ta classe: 
+  A) Archer
+  G) Guerrier
+  M) Magicien
+Votre choix: " 
+  else
+    print_string "
+Ta classe: 
+  A) Archere
+  G) Guerriere
+  M) Magicienne
+Votre choix: "
+  in 
+  let c =read_line()in
+  if not(c="A" || c="G" || c="M") then (print_string "il faut choisir une classe\n\n"; read_classe(g))
+  else c;;
+
+let rec init_aventure = fun ()->
+  let n = read_nom() in
+  let g = read_genre() in
+  let c = read_classe g in
+  print_string (n ^ g ^c)
+;;
 
