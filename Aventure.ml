bbc908918936aad3f9026c38291e799768a3cdd1
lysander051 <badet.maxime@gmail.com>
2022-04-13 15:14:58 +0200

    mise en place des exception

--- Aventure.ml
+++ Aventure.ml
@@ -6,4 +6,12 @@ Pour ce faire ton but est d'être la personne la plus préstigieuse au monde.
 Au faite qui es-tu aventurier?\n\n\n";;
 
 let personnage = GestionAventure.init_aventure();;
-GestionAventure.hubAventure personnage;;
+
+try 
+GestionAventure.hubAventure personnage
+
+with 
+| Personnage.Personnage.Personnage_mort -> print_string "Vous êtes mort\n\n"
+| Personnage.Personnage.Tue_En_Dormant _ -> print_string "Un monstre vous a tué dans la nuit\n\n"
+| GestionAventure.Quitte_le_jeu -> print_string "Votre personnage part à la retraite\n\n"
+;;
